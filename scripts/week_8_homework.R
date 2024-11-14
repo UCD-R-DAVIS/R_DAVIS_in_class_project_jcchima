7b4b9a967980af2d15d37d6064e106af90375b63
jcchima <jcchima@ucdavis.edu>
2024-11-14 12:00:33 -0800

    Week 8 Homework - Thurs Office Hours

--- scripts/week_8_homework.R
+++ scripts/week_8_homework.R
@@ -9,6 +9,7 @@ unique(gapminder$country)
 gapminder %>% 
   pivot_wider(names_from = "year", values_from = "country")
 
+#Test Space
 popdif <- gapminder %>% 
   group_by(year, country) %>% 
   summarize(pop) %>% 
@@ -16,17 +17,37 @@ popdif <- gapminder %>%
 
 #your want to get year and population
 test <- popdif %>% 
-  pivot_wider(names_from = "year", values_from = "pop")
+  pivot_wider(names_from = "year", values_from = "pop") %>% 
+  mutate(popdiff = `2007`-`2002`)
 
+test
 names(test)[2] <-"pop2002" 
 names(test)[3] <- "pop2007"
 
 
-gapminder %>% 
-  group_by(year, country) %>% 
-  summarize(pop) %>% 
-  filter(year >= 2002) %>% 
+popdif2 <- gapminder %>% 
+  filter(year >= 2002) %>%
+  select(country, continent, year, pop) %>% 
   pivot_wider(names_from = "year", values_from = "pop") %>% 
-  ggplot(mapping = aes(x = country, y =   , group = )) +
-  geom_bar() +
-  facet_wrap(~ country,)
+  mutate(popdiff = `2007`-`2002`)
+
+#Testing
+popdif3 <- gapminder %>% 
+  filter(year >= 2002) %>%
+  group_by(country, continent) %>% 
+  pivot_wider(names_from = "year", values_from = "pop") %>% 
+  summarize(popdiff = `2007`-`2002`)
+str(gapminder)# didn't work because group_by doesn't remove any columns
+
+#1. final
+gapminder %>% 
+  filter(year >= 2002, continent != "Oceania") %>%
+  select(country, continent, year, pop) %>% 
+  pivot_wider(names_from = "year", values_from = "pop") %>% 
+  mutate(popdiff = `2007`-`2002`) %>% 
+  ggplot(mapping = aes(x = reorder(country, popdiff), y =popdiff, group = continent)) +
+  geom_col(aes(fill = continent,)) +
+  facet_wrap(~continent, scales = "free") +
+  theme(axis.text.x = element_text(size = rel(0.9), angle = 90, vjust = 1, hjust=1))
+
+?element_text
